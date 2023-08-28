68d3ded483673a5e2e644d6367130b3eb0228118
Ajinkya Dawange <84762071+ajinkyadawange31045@users.noreply.github.com>
2023-08-28 11:24:06 +0530

    Update settings.py

--- study_space/settings.py
+++ study_space/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'pwfq77s4k7dqds#*(h$7n0f7ox#37$#iavfzzuqafb2ikc!x78'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['studyspace.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['studyspace.pythonanywhere.com','127.0.0.1','https://studypace.onrender.com/']
 
 
 # Application definition
